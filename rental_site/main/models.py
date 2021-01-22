7c1c0233d3bdfcded8733aeed757f358707e5ede
Smith-84 <go.dogala@gmail.com>
2021-01-22 16:56:07 +0700

    generic

--- rental_site/main/models.py
+++ rental_site/main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 # Create your models here.
 
 # товар, категория, тэги, продавец
