96a87c8e33bddb942672e8e5d7e54139ed876b38
Smith-84 <go.dogala@gmail.com>
2021-01-26 20:03:05 +0700

    Done

--- rental_site/main/views.py
+++ rental_site/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+
+class Index(View):
+    template_name = 'index.html'
+
+    def get(self, request):
+        turn_on_block = True
+        return render(request, self.template_name, context={'turn_on_block': turn_on_block})
