d63f5b2bc3f3a0f7567c9fa72d93f282aed67083
Smith-84 <go.dogala@gmail.com>
2021-02-06 16:26:36 +0700

    add new urls

--- rental_site/main/urls.py
+++ rental_site/main/urls.py
@@ -14,12 +14,16 @@ urlpatterns = [
     path('ad/<str:slug>/', AdDetail.as_view(), name='ad_detail'),
 
     path('accounts/', include('django.contrib.auth.urls')),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('dashboard/add/', RealtyList.as_view(), name='choice_type'),
+    path('accounts/profile/<int:pk>/', UserUpdate.as_view(), name='edit_user_profile'),
 
-    path('ad/add/<str:realty>/', add_realty_ad, name='add_new_ad'),
+    path('ad/add/apartment/', AddApartment.as_view(), name='add_apartment'),
+    path('ad/add/room/', AddRoom.as_view(), name='add_room'),
+    path('ad/add/garage/', AddGarage.as_view(), name='add_garage'),
+    path('ad/add/land-plot/', AddLandPlot.as_view(), name='add_land_plot'),
+
+    path('ad/add/photos/', SaveImages.as_view(), name='add_image'),
+
     path('ad/edit/<str:pk>/', EditRealtyAd.as_view(), name='edit_ad'),
-
-
-
-    path('accounts/profile/<int:pk>/', UserUpdate.as_view(), name='user_profile'),
-
 ]
