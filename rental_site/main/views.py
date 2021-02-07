e13602319b47bff018f2b49da30074f61c170ebe
Smith-84 <go.dogala@gmail.com>
2021-02-07 18:20:39 +0700

    ready 5.3

--- rental_site/main/views.py
+++ rental_site/main/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User, Group
 
 from .models import *
 from .forms import *
@@ -304,3 +305,9 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile.objects.create(user=instance)
         user_profile.save()
+
+
+@receiver(post_save, sender=User)
+def add_user_to_group(sender, instance, created, **kwargs):
+    common_users, _ = Group.objects.get_or_create(name="common_users")
+    instance.groups.add(common_users)
