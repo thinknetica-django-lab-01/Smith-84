8e1cd2237b15079a78e35911e1455fe46c2e9632
Smith-84 <go.dogala@gmail.com>
2021-02-11 16:32:44 +0700

    Delete hosts.py

--- rental_site/rental_site/hosts.py
+++
@@ -1,7 +0,0 @@
-from django_hosts import patterns, host
-from django.conf import settings
-
-host_patterns = patterns('',
-                         host(r'www', settings.ROOT_URLCONF, name='www'),
-                         host(r'(\w+)', 'main.urls', name='wildcard'),
-                         )
