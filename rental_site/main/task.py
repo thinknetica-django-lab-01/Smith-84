ac59307f40345fdc6be9c5ab0ab388c7b4264ce9
Smith-84 <go.dogala@gmail.com>
2021-02-14 15:55:35 +0700

    Done

--- rental_site/main/task.py
+++ rental_site/main/task.py
@@ -1,20 +1,21 @@
-from django.core.mail import send_mail, EmailMessage, get_connection
-from django.template.loader import render_to_string
-from .models import Subscribers, Ad
-from apscheduler.schedulers.background import BackgroundScheduler
 import datetime
 
-
-def get_message_body(ad_week):
-    return render_to_string(template_name='subscribe/subscribe_email.html', context={'ads': ad_week})
+from apscheduler.schedulers.background import BackgroundScheduler
+from .models import *
+from django.core.mail import send_mail, EmailMessage, get_connection
+from rental_site.celery import app
+from .utils import get_message_body
 
 
 def send_subscribers_new_ad():
+    """
+        Рассылка новых объявлений за неделю
+    """
     to_date = datetime.datetime.now()
     from_date = to_date - datetime.timedelta(7)
     subscribers = Subscribers.objects.all().values_list('email', flat=True)
-    ad_week = Ad.objects.filter(date_added__range=(from_date, to_date))
-    message_body = get_message_body(ad_week=ad_week)
+    ads_week = Ad.objects.filter(date_added__range=(from_date, to_date))
+    message_body = get_message_body(ads=ads_week)
     with get_connection() as connection:
         for subscriber in subscribers:
             email = EmailMessage(subject='Новое объявления',
@@ -26,9 +27,38 @@ def send_subscribers_new_ad():
 
 
 def start():
+    """
+        Планировщик рассылки сообщений за неделю
+    """
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=send_subscribers_new_ad, trigger='cron', day_of_week='mon', hour='0')
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
         pass
+
+
+@app.task
+def send_mail_new_users(email):
+    send_mail(
+        'Welcome',
+        'Here is the message.',
+        'admin@example.com',
+        [email],
+        fail_silently=False,
+    )
+    print('Отправил Welcome письмо', email)
+
+
+@app.task
+def send_subscribers_new_ads(subscribers, message):
+    with get_connection() as connection:
+        for subscriber in subscribers:
+            email = EmailMessage(subject='Новое объявление',
+                                 from_email='admin@example.com',
+                                 body=message,
+                                 to=[subscriber],
+                                 connection=connection)
+            email.content_subtype = "html"
+            email.send()
+            print('Отправил письмо', subscriber)
