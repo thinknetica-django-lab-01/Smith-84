f68634c0ebc9b74259238fc4891eec1cf39d251e
Smith-84 <go.dogala@gmail.com>
2021-02-15 23:58:32 +0700

    add redis cache

--- rental_site/rental_site/settings.py
+++ rental_site/rental_site/settings.py
@@ -85,7 +85,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rental_site.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -95,7 +94,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -115,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -129,7 +126,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -140,7 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = "/dashboard/"
-
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -183,6 +178,16 @@ CELERY_TIMEZONE = 'Asia/Krasnoyarsk'
 CELERY_BEAT_SCHEDULE = {
     "week_mailing": {
         "task": "week_mailing",
-        "schedule":  crontab(hour=0, minute=30, day_of_week=1)
+        "schedule": crontab(hour=0, minute=30, day_of_week=1)
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis: //127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
