d55865d32e5f00eb88b34b41d8c2be18ca5a6179
Smith-84 <go.dogala@gmail.com>
2021-03-06 13:57:54 +0700

    Done

--- rental_site/main/utils.py
+++ rental_site/main/utils.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 
 def check_mobile_ua(request):
-    """Возвращает True если request мобильного девайса"""
+    """Возвращает True если request мобильного девайса."""
 
     mobile_agent_re = re.compile(r".*(iphone|mobile|androidtouch)", re.IGNORECASE)
     if mobile_agent_re.match(request.META['HTTP_USER_AGENT']):
